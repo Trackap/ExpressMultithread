b70dbb3cb7efa9ed4686f4eca6a82fd2103b6b8a
Maxime Deroulou <maxime.deroulou@epitech.eu>
2023-08-02 11:42:02 +0200

    strings: add more constants

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,6 +1,10 @@
 /* Characters */
 export const slash = "/";
 export const empty = "";
+export const nl = "\n";
+export const space = " ";
+export const bRight = "[";
+export const bLeft = "]";
 
 /* Keys */
 export const defaultStr = "default";
@@ -13,9 +17,24 @@ export const node_modules = "node_modules";
 export const childFile = "Child.js";
 
 /* Workers */
-export const ready = "ready";
-export const response = "response";
+export const message = "message";
+export const error = "error";
 
 /* Messages */
 export const redefinition = "Redefinition of route ";
 export const childNotFound = "Child not found";
+export const noMain = "This file is not meant to be run in the main thread.";
+export const noParentPort = "No parent port";
+export const unknownCmd = "Unknown command";
+
+/* Console */
+export const Reset = "\x1b[0m"
+export const FgGray = "\x1b[90m"
+export const FgCyan = "\x1b[36m"
+export const FgYellow = "\x1b[33m"
+export const FgRed = "\x1b[31m"
+
+export const logConsole = "[L]";
+export const infoConsole = "[I]";
+export const warnConsole = "[W]";
+export const errorConsole = "[E]";
