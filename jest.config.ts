f80f451df563342c5ce3397397bb4ff7382abd72
Maxime Deroulou <maxime.deroulou@epitech.eu>
2023-08-08 09:58:32 +0200

    jest.config: add exclude path

--- jest.config.ts
+++ jest.config.ts
@@ -6,12 +6,15 @@ const config: Config = {
     transform:{
         "^.+\\Child.[t|j]s?$": "workerloader-jest-transformer"
     },
+    testRegex: ".spec.[t|j]s",
     testEnvironment: 'node',
     forceExit: true,
     detectOpenHandles: true,
     collectCoverage: true,
-    collectCoverageFrom: [
-        "src/**/*.{ts,js}",
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        "/test/",
+        "/dist/"
     ],
     coverageReporters: [
         "html",
