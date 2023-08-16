beeb2dc0e1ccf40a6f5b06d826a5b7a0f7f71edc
Maxime Deroulou <maxime.deroulou@epitech.eu>
2023-08-16 17:04:39 +0200

    child.js: cleaner catch & clean file

--- src/class/Child.js
+++ src/class/Child.js
@@ -1,11 +1,7 @@
 const path = require('path');
 require('ts-node').register();
-// register({
-//     project: path.resolve(process.cwd(), 'tsconfig.json')
-// });
-const { ChildError } =  require(path.resolve(__filename.replace(".js", "Error.ts")));
 try {
     require(path.resolve(__filename.replace(".js", ".ts")));
 } catch (e) {
-    throw new ChildError(e.diagnosticText, e.diagnosticCodes);
+    throw new Error(e);
 }
