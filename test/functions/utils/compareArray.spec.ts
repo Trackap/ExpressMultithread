23de897973e00728a98aaddc84215eb7bd75823c
Maxime Deroulou <maxime.deroulou@epitech.eu>
2023-08-17 10:08:13 +0200

    compareArray test: add test suite

---
+++ test/functions/utils/compareArray.spec.ts
@@ -0,0 +1,25 @@
+import { compareArray } from '../../../src/functions/utils/compareArray';
+import { expect } from 'chai';
+
+describe('compareArray tests', () => {
+    it("should return true", () => {
+        const arr = [1, 2, 3];
+        expect(compareArray([1, 2, 3], [1, 2, 3])).to.be.true;
+        expect(compareArray(["1", "2", "3"], ["1", "2", "3"])).to.be.true;
+        expect(compareArray([true, false], [true, false])).to.be.true;
+        expect(compareArray([arr], [arr])).to.be.true;
+        expect(compareArray([], [])).to.be.true;
+        expect(compareArray([arr], [Object.create(arr).reverse()])).to.be.true;
+
+    });
+
+    it("should return false", () => {
+        const arr = [1, 2, 3];
+        expect(compareArray([1, 2, 3], [1, 2])).to.be.false;
+        expect(compareArray(["1", "2", "3"], ["1", "2", "4"])).to.be.false;
+        expect(compareArray([true, false], [false, true])).to.be.false;
+        expect(compareArray([[]], [[], []])).to.be.false;
+        expect(compareArray(arr, Object.create(arr).reverse())).to.be.false;
+
+    });
+});
