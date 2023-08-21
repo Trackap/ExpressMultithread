6e24601d51be655f6e564b17a434debac52a0506
Maxime Deroulou <maxime.deroulou@epitech.eu>
2023-08-21 14:16:13 +0200

    importModule: add resolve based on cwd on the import

--- src/functions/utils/importModule.ts
+++ src/functions/utils/importModule.ts
@@ -1,6 +1,12 @@
+/* Modules */
+import { resolve } from "path";
+
+/* Constants */
+const cwd = process.cwd();
+
 export const importModule = (path : string, debug: boolean = true) : any => {
     try {
-        return require(path);
+        return require(resolve(cwd, path));
     } catch (e) {
         debug && console.error(e);
         return {};
