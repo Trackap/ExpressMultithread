008908326ae3406d92f11b587ce0b87229410188
Maxime Deroulou <maxime.deroulou@epitech.eu>
2023-09-20 22:43:15 +0200

    routeDecorator: avoid get set named method error

--- src/functions/decorators/route.ts
+++ src/functions/decorators/route.ts
@@ -2,10 +2,12 @@
 import { RouteMethod, Middleware } from "../../types";
 
 /* Constants */
-import { __route } from "../../constants/strings";
+import {__route, get, keywordUse, set} from "../../constants/strings";
 
 export function route(method: RouteMethod, path: string, ...middlewares: Middleware[]) {
     return (target: any, propertyKey: string) => {
+        if (propertyKey === get || propertyKey === set)
+            throw new Error(keywordUse);
         Object.defineProperty(target, propertyKey + __route, {value: {method, path, middlewares, cb: target[propertyKey]}});
         return target;
     };
