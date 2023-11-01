afa08e254a99692f7b81ad8354b59ff57b8f7506
Maxime D <maxime.deroulou@epitech.eu>
2023-11-01 17:44:22 +0100

    config: fix 0 threadCount in config not handled

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -16,7 +16,7 @@ import { cfgFile } from "../constants/strings";
 
 const userCfg = importModule(cfgFile, false)?.default as BaseConfig | undefined;
 const cfg = {
-    threadCount: userCfg?.threadCount ?? (process.env.THREAD_COUNT === undefined ? cpus().length : parseInt(process.env.THREAD_COUNT)),
+    threadCount: (userCfg?.threadCount !== undefined ? userCfg?.threadCount : (process.env.THREAD_COUNT === undefined ? cpus().length : parseInt(process.env.THREAD_COUNT))),
     cleanRequest: userCfg?.cleanRequest ? (req: Request) => merge(userCfg!.cleanRequest!(req), cleanRequest(req)) : cleanRequest,
     plugins: importPlugin(userCfg?.plugins ?? []),
     overrideConsole: userCfg?.overrideConsole ?? true,
