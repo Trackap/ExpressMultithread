400bcb0bb058ac6491bb871704672a91341cc37f
Maxime Deroulou <maxime.deroulou@epitech.eu>
2024-04-09 17:34:48 +0200

    index: add more exported elems

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+/* Multithreaded Router */
 import { Multithreaded } from './class/Router';
 export { Multithreaded };
+
+/* Route & controller decorators */
+export * from "./decorators";
+
+/* Plugin class */
+export * from "./class/Plugin";
+
+/* Types */
+export type {
+    BaseConfig,
+    InternalRoute,
+    ControllerDecoratorOpts,
+    ObjectPrototype,
+    PluginRoute,
+    PluginType,
+    PluginController,
+} from "./types";
+
 export default Multithreaded;
